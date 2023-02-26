66648fa0ca45399c0f2b4a0f2e850960f3750823
dvhoang <dvhlm10@gmail.com>
2023-02-26 09:52:28 +0700

    convert image processing with Firebase to useFirebaseImage hook

---
+++ src/hooks/useFirebaseImage.js
@@ -0,0 +1,73 @@
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+export default function useFirebaseImage(setValue, getValues) {
+  const [image, setImage] = useState("");
+  const [progress, setProgress] = useState(0);
+  const handleUploadImage = (file) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progress);
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            console.log("Nothing at all");
+        }
+      },
+      (error) => {
+        console.log("Error");
+        setImage("");
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          setImage(downloadURL);
+        });
+      }
+    );
+  };
+  const handleSelectImage = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image_name", file.name);
+    handleUploadImage(file);
+  };
+  const handleDeleteImage = () => {
+    const storage = getStorage();
+    const imageRef = ref(storage, "images/" + getValues("image_name"));
+    deleteObject(imageRef)
+      .then(() => {
+        toast.success("Delete image succesfully");
+        setImage("");
+        setProgress(0);
+      })
+      .catch((error) => {
+        toast.error("Can not delete image");
+      });
+  };
+  return {
+    image,
+    setImage,
+    progress,
+    handleSelectImage,
+    handleDeleteImage,
+  };
+}
--- src/modules/post/PostAddNew.js
+++ src/modules/post/PostAddNew.js
@@ -18,13 +18,7 @@ import * as yup from "yup";
 import { toast } from "react-toastify";
 import slugify from "slugify";
 import ImageUpload from "components/image/ImageUpload";
-import {
-  getStorage,
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-  deleteObject,
-} from "firebase/storage";
+import useFirebaseImage from "hooks/useFirebaseImage";
 
 const schema = yup.object({
   title: yup.string().required("Please enter your title"),
@@ -54,60 +48,8 @@ const PostAddNew = () => {
   });
   const watchStatus = watch("status");
   const watchHot = watch("hot");
-  const [progress, setProgress] = useState(0);
-  const [image, setImage] = useState("");
-  const handleUploadImage = (file) => {
-    const storage = getStorage();
-    const storageRef = ref(storage, "images/" + file.name);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setProgress(progress);
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-          default:
-            console.log("Nothing at all");
-        }
-      },
-      (error) => {
-        console.log("Error");
-        setImage("");
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          console.log("File available at", downloadURL);
-          setImage(downloadURL);
-        });
-      }
-    );
-  };
-  const handleSelectImage = (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-    setValue("image_name", file.name);
-    handleUploadImage(file);
-  };
-  const handleDeleteImage = () => {
-    const storage = getStorage();
-    const imageRef = ref(storage, "images/" + getValues("image_name"));
-    deleteObject(imageRef)
-      .then(() => {
-        toast.success("Delete image succesfully");
-        setImage("");
-        setProgress(0);
-      })
-      .catch((error) => {
-        toast.error("Can not delete image");
-      });
-  };
+  const { image, progress, handleSelectImage, handleDeleteImage } =
+    useFirebaseImage(setValue, getValues);
   useEffect(() => {
     const arrErrors = Object.values(errors);
     if (arrErrors.length > 0) {
